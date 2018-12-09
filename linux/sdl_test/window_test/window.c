7ae8a968dd03ca5197f2b844180d506d4fffd2b8
Luke Brady <lbrad23105@gmail.com>
2018-12-09 13:20:47 -0500

    Added a renderer and a bmp background image to the window.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- linux/sdl_test/window_test/window.c
+++ linux/sdl_test/window_test/window.c
@@ -3,7 +3,10 @@
 
 int main() {
 	// Declare a pointer and initialize SDL2.
-	SDL_Window *window;
+	SDL_Window *window = NULL;
+	SDL_Renderer *renderer = NULL;
+	SDL_Texture *bitmapTex = NULL;
+	SDL_Surface *bitmapSurface = NULL;
 	SDL_Init(SDL_INIT_VIDEO);
 
 	// Create the application window.
@@ -11,8 +14,8 @@ int main() {
 		"My test SDL Window.",
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
-		640,
-		480,
+		500,
+		319,
 		SDL_WINDOW_OPENGL
 	);
 
@@ -21,11 +24,37 @@ int main() {
 		return 1;
 	}
 
+	// Create the renderer.
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if(renderer == NULL) {
+		printf("Could not create the SDL renderer.\n");
+		return 1;
+	}
+	// Load the background image.
+	bitmapSurface = SDL_LoadBMP("img/background.bmp");
+	bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
+	SDL_FreeSurface(bitmapSurface);
+
 	// Create the program loop here.
-	
-	SDL_Delay(3000);
+	while(1) {
+		SDL_Event e;
+		if(SDL_PollEvent(&e)) {
+			if(e.type == SDL_QUIT) {
+				break;
+			}
+		}
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	// Destroy the texture, renderer, and window.
+	SDL_DestroyTexture(bitmapTex);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 
 	SDL_Quit();
+
 	return 0;
 }
