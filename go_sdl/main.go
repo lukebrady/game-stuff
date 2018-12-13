c3a28489a3d5bb3375a5eda9da35a724ea75ce65
Luke Brady <lbrad23105@gmail.com>
2018-12-13 14:08:10 -0500

    Messing around with audio files.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- go_sdl/main.go
+++ go_sdl/main.go
@@ -1,8 +1,45 @@
 package main
 
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"io/ioutil"
+	"log"
+	"runtime"
+
+	"github.com/veandco/go-sdl2/mix"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func playAudio() {
+	if err := mix.OpenAudio(44100, mix.DEFAULT_FORMAT, 2, 4096); err != nil {
+		log.Println(err)
+		return
+	}
+	defer mix.CloseAudio()
+
+	// Load entire WAV data from file
+	data, err := ioutil.ReadFile("/Users/ltbrady/Desktop/Media/itat19.wav")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Load WAV from data (memory)
+	chunk, err := mix.QuickLoadWAV(data)
+	if err != nil {
+		log.Println(err)
+	}
+	defer chunk.Free()
+
+	// Play 4 times
+	chunk.Play(1, 3)
+
+	// Wait until it finishes playing
+	for mix.Playing(-1) == 1 {
+		sdl.Delay(16)
+	}
+}
 
 func main() {
+	runtime.LockOSThread()
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
@@ -14,7 +51,7 @@ func main() {
 		panic(err)
 	}
 	defer window.Destroy()
-
+	go playAudio()
 	running := true
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
